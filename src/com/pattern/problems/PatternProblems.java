a83de1b54a24f906effc7de89000a41aa6d90a05
Manish <singh.manish7@outlook.in>
2022-09-25 20:28:49 +0530

    Updated Pattern Java File

--- src/com/pattern/problems/PatternProblems.java
+++ src/com/pattern/problems/PatternProblems.java
@@ -4,14 +4,19 @@ public class PatternProblems {
 
 	public static void main(String[] args) {
 
-		squarePattern(5);
+	//	squarePattern(5);
+	//	increasingPattern(5);
+	//	increasingPatternNumber(5);
+	//	rightTrianglePatternNumber(5);
 
 	}
 
 	/***
 	 * 
 	 * @param number
+	 * 
 	 */
+	
 	private static void squarePattern(int number) {
 		System.out.println("Entering method squarePattern");
 
@@ -26,16 +31,57 @@ public class PatternProblems {
 		System.out.println("Exiting method squarePattern");
 	}
 	
+	/***
+	 * 
+	 * @param number
+	 */
 	private static void increasingPattern(int number) {
 		System.out.println("Entering method increasingPattern");
-		int tmp = 0;
 		
-		for(int i = 0 ; i < number; i++) {
-			while(tmp == number) {
-				System.out.println();
+		for(int i = 0 ; i < number ; i++) {
+			for(int j = 0; j <=i ; j++) {
+				System.out.print("*");
 			}
+			System.out.println();
 		}
 		
 		System.out.println("Exiting method increasingPattern");
 	}
+	
+	/***
+	 * 
+	 * @param number
+	 */
+	private static void increasingPatternNumber(int number) {
+		System.out.println("Entering method increasingPatternNumber");
+		
+		for(int i = 0 ; i < number ; i++) {
+			for(int j = 0; j <=i ; j++) {
+				System.out.print(i+1);
+			}
+			System.out.println();
+		}
+		
+		System.out.println("Exiting method increasingPatternNumber");
+	}
+	
+	/***
+	 * 
+	 * @param number
+	 */
+	private static void rightTrianglePatternNumber(int number) {
+		System.out.println("Entering method rightTrianglePatternNumber");
+		
+		for(int i = 0 ; i < number ; i++) {
+			int num = 1;
+			for(int j = 0; j <=i ; j++) {
+				System.out.print(num);
+				num++;
+			}
+			System.out.println();
+		}	
+		System.out.println("Exiting method rightTrianglePatternNumber");
+	}
+	
+	
 }
